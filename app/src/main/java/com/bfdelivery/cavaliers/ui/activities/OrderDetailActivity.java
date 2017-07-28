0e71f930e2aab3a1af1d282f5c090876624075d4
kapaseker <panoogunker@gmail.com>
2017-07-28 18:12:24 +0800

    完成订单详情中的商品列表

--- app/src/main/java/com/bfdelivery/cavaliers/ui/activities/OrderDetailActivity.java
+++ app/src/main/java/com/bfdelivery/cavaliers/ui/activities/OrderDetailActivity.java
@@ -1,11 +1,18 @@
 package com.bfdelivery.cavaliers.ui.activities;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.bfdelivery.cavaliers.R;
 import com.bfdelivery.cavaliers.ui.activities.base.BasePageActivity;
 
 public class OrderDetailActivity extends BasePageActivity {
+
+	ListView mListCommodity = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,7 @@ public class OrderDetailActivity extends BasePageActivity {
 
 	@Override
 	protected void initView() {
-
+		mListCommodity = (ListView) findViewById(R.id.lstCommodities);
 	}
 
 	@Override
@@ -29,6 +36,36 @@ public class OrderDetailActivity extends BasePageActivity {
 
 	@Override
 	protected void processViewAndData() {
+		mListCommodity.setAdapter(new CommodityAdapter());
+	}
 
+	private static class CommodityAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return 7;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			if (convertView == null) {
+				convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_commodity_item, parent, false);
+			} else {
+
+			}
+
+			return convertView;
+		}
 	}
 }
---
+++ app/src/main/java/com/bfdelivery/cavaliers/ui/views/NoScrollListView.java
@@ -0,0 +1,36 @@
+package com.bfdelivery.cavaliers.ui.views;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by Panoo on 2017/7/28.
+ */
+
+public class NoScrollListView extends ListView {
+	public NoScrollListView(Context context) {
+		super(context);
+	}
+
+	public NoScrollListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public NoScrollListView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	public NoScrollListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, expandSpec);
+	}
+}
