c7fa238cdd228adc4bb6f7b29dce41e0d2805dfc
kapaseker <panoogunker@gmail.com>
2017-07-31 15:23:27 +0800

    每日结算页面的相关效果

--- app/src/main/java/com/bfdelivery/cavaliers/ui/activities/DailySettleActivity.java
+++ app/src/main/java/com/bfdelivery/cavaliers/ui/activities/DailySettleActivity.java
@@ -1,48 +1,81 @@
 package com.bfdelivery.cavaliers.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.bfdelivery.cavaliers.R;
 import com.bfdelivery.cavaliers.ui.activities.base.BasePageActivity;
 
 public class DailySettleActivity extends BasePageActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-	    getSupportActionBar().setTitle(R.string.action_menu_daily_settle);
-    }
+	RecyclerView mLstOrder = null;
 
-    @Override
-    protected void onPrepareLayout() {
-	    setContentView(R.layout.activity_daily_settle);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getSupportActionBar().setTitle(R.string.action_menu_daily_settle);
+	}
 
-    @Override
-    protected void initView() {
+	@Override
+	protected void onPrepareLayout() {
+		setContentView(R.layout.activity_daily_settle);
+	}
 
-    }
+	@Override
+	protected void initView() {
+		mLstOrder = (RecyclerView) findViewById(R.id.lst_daily);
+		mLstOrder.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+		mLstOrder.setHasFixedSize(true);
+	}
 
-    @Override
-    protected void handleData(Bundle data) {
+	@Override
+	protected void handleData(Bundle data) {
 
-    }
+	}
 
-    @Override
-    protected void processViewAndData() {
+	@Override
+	protected void processViewAndData() {
+		mLstOrder.setAdapter(new DailyOrderItemAdapter());
+	}
 
-    }
+	private static final class DailyOrderItemAdapter extends RecyclerView.Adapter<DailyOrderItemAdapter.DailyOrderItemViewHolder> {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.dailysettle,menu);
-        return true;
-    }
+		public static class DailyOrderItemViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
+			public DailyOrderItemViewHolder(View itemView) {
+				super(itemView);
+			}
+
+			public void bindData(int position) {
+				itemView.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						itemView.getContext().startActivity(new Intent(itemView.getContext(), OrderDetailActivity.class));
+					}
+				});
+			}
+		}
+
+		@Override
+		public DailyOrderItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_daily_order_item, parent, false);
+			return new DailyOrderItemViewHolder(itemView);
+		}
+
+		@Override
+		public void onBindViewHolder(DailyOrderItemViewHolder holder, int position) {
+			holder.bindData(position);
+		}
+
+		@Override
+		public int getItemCount() {
+			return 12;
+		}
+
+	}
 }
