bbd0ed98ad02847a240799ec6d4e253301599ff5
kapaseker <panoogunker@gmail.com>
2017-08-04 17:51:38 +0800

    添加配送的Application

---
+++ app/src/main/java/com/bfdelivery/cavaliers/ui/config/CavApplication.java
@@ -0,0 +1,14 @@
+package com.bfdelivery.cavaliers.ui.config;
+
+import android.app.Application;
+
+/**
+ * Created by Panoo on 2017/8/4.
+ */
+
+public class CavApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
