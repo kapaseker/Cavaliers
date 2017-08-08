e854888442a7ab75c6f11791fe2fae1025970787
kapaseker <panoogunker@gmail.com>
2017-08-08 18:34:02 +0800

    每日结算界面的修正

--- app/src/main/java/com/bfdelivery/cavaliers/ui/activities/DailySettleActivity.java
+++ app/src/main/java/com/bfdelivery/cavaliers/ui/activities/DailySettleActivity.java
@@ -1,19 +1,12 @@
 package com.bfdelivery.cavaliers.ui.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.bfdelivery.cavaliers.R;
 import com.bfdelivery.cavaliers.ui.activities.base.BasePageActivity;
 
 public class DailySettleActivity extends BasePageActivity {
 
-	RecyclerView mLstOrder = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +21,6 @@ public class DailySettleActivity extends BasePageActivity {
 
 	@Override
 	protected void initView() {
-		mLstOrder = (RecyclerView) findViewById(R.id.lst_daily);
-		mLstOrder.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-		mLstOrder.setHasFixedSize(true);
 	}
 
 	@Override
@@ -40,42 +30,7 @@ public class DailySettleActivity extends BasePageActivity {
 
 	@Override
 	protected void processViewAndData() {
-		mLstOrder.setAdapter(new DailyOrderItemAdapter());
-	}
-
-	private static final class DailyOrderItemAdapter extends RecyclerView.Adapter<DailyOrderItemAdapter.DailyOrderItemViewHolder> {
-
-		public static class DailyOrderItemViewHolder extends RecyclerView.ViewHolder {
-
-			public DailyOrderItemViewHolder(View itemView) {
-				super(itemView);
-			}
-
-			public void bindData(int position) {
-				itemView.setOnClickListener(new View.OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						itemView.getContext().startActivity(new Intent(itemView.getContext(), OrderDetailActivity.class));
-					}
-				});
-			}
-		}
-
-		@Override
-		public DailyOrderItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-			View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_daily_order_item, parent, false);
-			return new DailyOrderItemViewHolder(itemView);
-		}
-
-		@Override
-		public void onBindViewHolder(DailyOrderItemViewHolder holder, int position) {
-			holder.bindData(position);
-		}
-
-		@Override
-		public int getItemCount() {
-			return 12;
-		}
 
 	}
+
 }
